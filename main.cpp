8b34c44814f3902ca5bd9f680c50d4cb3a285129
fedor2076 <kot_fed@mail.ru>
2023-09-28 13:56:19 +0400

    two commit

--- main.cpp
+++ main.cpp
@@ -1,23 +1,17 @@
-//Intel Core i3 3220 @ 3.30GHz два ядра 4 потока.
-/*
+/*Intel Core i3 3220 @ 3.30GHz два ядра 4 потока.
+
 при разбивке на два потока происходит самое быстрое вычисление
 
 */
-
-
 
 #include <vector>
 #include <iostream>
 #include <chrono>
 #include <random>
 #include <thread>
-//#include <cstdlib>
-
-
 
 void sum(std::vector<int> myVector, int l, int r)
-{
-  
+{  
     int s = 0;
     for (int i = l; i < r; i++)
     {
@@ -29,19 +23,14 @@ void sum(std::vector<int> myVector, int l, int r)
 int all = 0;
 
 void sum_(std::vector<int> myVector, int l, int r)
-{
-    
+{    
     int s = 0;
     for (int i = l; i < r; i++)
     {
         s += myVector[i];
     }
-   
-     all += s;
+      all += s;
 }
-
-
-
 
 int main()
 {
@@ -58,6 +47,7 @@ int main()
     std::cout << std::endl;
     int l = 0;
     int r = 0;
+   
     //вычисление без потоков
     auto timePoint1 = std::chrono::steady_clock::now();
     sum(myVector, 0, size);
@@ -73,7 +63,7 @@ int main()
     timePoint1 = std::chrono::steady_clock::now();
     for (int i = 0; i < quantity_t; i++)
     {
-          l = r;
+        l = r;
         r = (i + 1) * n;
         std::thread t(&sum_, myVector, l, r);
         threads.push_back(std::move(t));
@@ -84,6 +74,7 @@ int main()
         if (t.joinable())
             t.join();
     }
+    
     std::cout << all << "\n\n";
     timePoint2 = std::chrono::steady_clock::now();
     duration = std::chrono::duration_cast<std::chrono::milliseconds> (timePoint2 - timePoint1).count();
